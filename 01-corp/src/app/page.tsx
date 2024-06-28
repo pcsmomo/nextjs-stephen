c14338ef62957173a8b95e304c3321a14b6aac57
Noah Kim <dongwoo_333@hanmail.net>
2024-06-29 06:41:01 +1000

    10. Adding Images

--- 01-corp/src/app/page.tsx
+++ 01-corp/src/app/page.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+// import HomeImg from "@public/home.jpg";
+import HomeImg from "public/home.jpg";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={HomeImg}
+          alt="car factory"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
