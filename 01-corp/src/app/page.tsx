2a3ade32ae5e619114384d151f61d4af306cbf89
Noah Kim <dongwoo_333@hanmail.net>
2024-06-30 07:03:32 +1000

    12. Adding a Reusable Presentation Component

--- 01-corp/src/app/page.tsx
+++ 01-corp/src/app/page.tsx
@@ -1,23 +1,12 @@
-import Image from 'next/image';
-// import HomeImg from "@public/home.jpg";
 import HomeImg from 'public/home.jpg';
+import Hero from '@/components/herok';
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={HomeImg}
-          alt="car factory"
-          fill
-          style={{ objectFit: 'cover' }}
-        />
-      </div>
-      {/* <div className="flex flex-row">
-        <Image height={800} width={600} src={HomeImg} alt="car asdf" />
-        <div>Buy our product</div>
-      </div> */}
-    </div>
+    <Hero
+      imgData={HomeImg}
+      imgAlt="car factory"
+      title="Professional Cloud Hosting"
+    />
   );
 }
