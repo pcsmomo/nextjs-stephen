c52190547c49e5e3af994d316e83738b691af155
Noah Kim <dongwoo_333@hanmail.net>
2024-07-12 18:25:17 +1000

    13. Adding Some Styling

--- 01-corp/src/app/page.tsx
+++ 01-corp/src/app/page.tsx
@@ -1,10 +1,10 @@
-import HomeImg from 'public/home.jpg';
-import Hero from '@/components/herok';
+import homeImg from 'public/home.jpg';
+import Hero from '@/components/hero';
 
 export default function Home() {
   return (
     <Hero
-      imgData={HomeImg}
+      imgData={homeImg}
       imgAlt="car factory"
       title="Professional Cloud Hosting"
     />
