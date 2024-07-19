14eab6f189fadbd4663a9678ab77efd1bb5715bb
Noah Kim <dongwoo_333@hanmail.net>
2024-07-19 14:48:02 +1000

    42. UseForm State in Action

--- 02-snippets/src/actions/index.ts
+++ 02-snippets/src/actions/index.ts
@@ -19,3 +19,26 @@ export async function deleteSnippet(id: number) {
 
   redirect('/');
 }
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  return {
+    message: 'Title must be longer',
+  };
+  // // Check the user's inputs and make sure they're valid
+  // const title = formData.get('title') as string;
+  // const code = formData.get('code') as string;
+
+  // // Create a new record in the database
+  // const snippet = await db.snippet.create({
+  //   data: {
+  //     title,
+  //     code,
+  //   },
+  // });
+
+  // // Redirect the user back to the root route
+  // redirect('/');
+}
