43fca8f27ed19937d25761d9107e6e8eedd8b5d8
Noah Kim <dongwoo_333@hanmail.net>
2024-07-28 08:29:43 +1000

    116. Receiving the Query String in a Server Component

---
+++ 03-discuss/src/app/search/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from 'next/navigation';
+
+interface SearchPageProps {
+  searchParams: {
+    term: string;
+  };
+}
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { term } = searchParams;
+
+  if (!term) {
+    redirect('/');
+  }
+
+  return <div>{term}</div>;
+}
